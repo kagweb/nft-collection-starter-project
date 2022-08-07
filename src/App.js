5b39ac3400f221c723c5fd79b220868d062294f1
kagweb <kag.web@gmail.com>
2022-08-07 19:28:03 +0900

    Finished section-4-Lesson-1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
+import { ethers } from "ethers";
+import myEpicNft from "./utils/MyEpicNFT.json";
 
 // Constantsを宣言する: constとは値書き換えを禁止した変数を宣言する方法です。
 const TWITTER_HANDLE = "kagweb3";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0x79e3bbf75a4e843ae766615e75f9a0A4720b2Ac6";
+const CHAIN_ID = "0x5"; // Goerli Test Network
 
 const App = () => {
   // ユーザーのウォレットアドレスを格納するために使用する状態変数を定義します。
   const [currentAccount, setCurrentAccount] = useState("");
+  const [isMinting, setIsMinting] = useState(false);
+
+  console.log("currentAccount", currentAccount);
+
+  // MyEpicNFT.sol の中で event が　emit された時に情報を受け取ります。
+  const setupEventListener = useCallback(async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        // NFT が発行されます。
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+        // Event が　emit される際に、コントラクトから送信される情報を受け取っています。
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(
+            `あなたのウォレットに NFT を送信しました。OpenSea に表示されるまで最大で10分かかることがあります。NFT へのリンクはこちらです: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   // ユーザーがMetaMaskを持っているか確認します。
-  const checkIfWalletIsConnected = async () => {
+  const checkIfWalletIsConnected = useCallback(async () => {
     const { ethereum } = window;
     if (!ethereum) {
       console.log("Make sure you have MetaMask!");
@@ -32,21 +68,109 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+      // イベントリスナーをここで設定
+      // この時点で、ユーザーはウォレット接続が済んでいます。
+      setupEventListener();
     } else {
       console.log("No authorized account found");
     }
+  }, [setupEventListener]);
+
+  // connectWallet メソッドを実装します。
+  const connectWallet = useCallback(async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+      // ウォレットアドレスに対してアクセスをリクエストしています。
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+
+      // ウォレットアドレスを currentAccount に紐付けます。
+      setCurrentAccount(accounts[0]);
+
+      // **** イベントリスナーをここで設定 ****
+      setupEventListener();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [setupEventListener]);
+
+  // NFT を Mint する関数を定義しています。
+  const askContractToMintNft = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+        console.log("Going to pop wallet now to pay gas...");
+        const nftTxn = await connectedContract.makeAnEpicNFT();
+        setIsMinting(true);
+        console.log("Mining...please wait.");
+        await nftTxn.wait();
+        setIsMinting(false);
+
+        console.log(
+          `Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`
+        );
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
-  // renderNotConnectedContainer メソッドを定義します。
-  const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
-      Connect to Wallet
-    </button>
-  );
+
+  const checkChainId = useCallback(async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const chainId = await ethereum.request({ method: "eth_chainId" });
+        console.log("Connected to chain " + chainId);
+        if (chainId !== CHAIN_ID) {
+          alert("You are not connected to the Goerli Test Network!");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   // ページがロードされたときに useEffect()内の関数が呼び出されます。
   useEffect(() => {
     checkIfWalletIsConnected();
-  }, []);
+    checkChainId();
+  }, [checkIfWalletIsConnected, checkChainId]);
+
+  // renderNotConnectedContainer メソッドを定義します。
+  const renderNotConnectedContainer = () => (
+    <button
+      onClick={connectWallet}
+      className="cta-button connect-wallet-button"
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  // Mint NFT ボタンをレンダリングするメソッドを定義します。
+  const renderMintUI = () => (
+    <button
+      onClick={askContractToMintNft}
+      className="cta-button connect-wallet-button"
+    >
+      {isMinting ? "Mining...please wait." : "Mint NFT"}
+    </button>
+  );
 
   return (
     <div className="App">
@@ -57,13 +181,9 @@ const App = () => {
           {/* 条件付きレンダリングを追加しました
           // すでに接続されている場合は、
           // Connect to Walletを表示しないようにします。*/}
-          {currentAccount === "" ? (
-            renderNotConnectedContainer()
-          ) : (
-            <button onClick={null} className="cta-button connect-wallet-button">
-              Mint NFT
-            </button>
-          )}
+          {currentAccount === ""
+            ? renderNotConnectedContainer()
+            : renderMintUI()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
